533e471ffe0b5ce3394338661efe12382dd3f872
code-space <84518130+Raskan07@users.noreply.github.com>
2024-11-16 06:26:06 +0000

    theme updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/app/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-            <ThemeProvider
+      <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
